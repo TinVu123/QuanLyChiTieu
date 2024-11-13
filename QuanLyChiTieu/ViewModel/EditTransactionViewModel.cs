428bcb21ca7b63538033bfc5c642d63a313400c9
Hanh <nguyenminhhanh1005@gmail.com>
2024-11-13 19:47:44 +0700

    chức năng sửa và đăng xuất

---
+++ QuanLyChiTieu/ViewModel/EditTransactionViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using QuanLyChiTieu.Models;
+
+namespace QuanLyChiTieu.ViewModel
+{
+    public class EditTransactionViewModel : BaseViewModel
+    {
+        public ICommand SaveCommand { get; }
+        public ICommand CancelCommand { get; }
+
+        private Transaction _transaction;
+        public Transaction Transaction
+        {
+            get => _transaction;
+            set
+            {
+                _transaction = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public EditTransactionViewModel(Transaction transaction)
+        {
+            Transaction = transaction;
+
+            SaveCommand = new RelayCommand<object>(null, SaveTransaction);
+            CancelCommand = new RelayCommand<object>(null, Cancel);
+        }
+
+        private void SaveTransaction(object parameter)
+        {
+            MessageBox.Show($"Đã lưu thay đổi cho giao dịch: {Transaction.Description}");
+
+        }
+
+        private void Cancel(object parameter)
+        {
+            MessageBox.Show("Hủy bỏ chỉnh sửa");
+        }
+    }
+}
---
+++ QuanLyChiTieu/ViewModel/HomeViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using QuanLyChiTieu.Models;
+using System.Windows;
+using QuanLyChiTieu.View;
+
+
+namespace QuanLyChiTieu.ViewModel
+{
+    public class HomeViewModel : BaseViewModel
+    {
+        public ICommand LogoutCommand { get; }
+        public ICommand EditCommand { get; }
+
+        private Transaction _selectedTransaction;
+        public Transaction SelectedTransaction
+        {
+            get => _selectedTransaction;
+            set
+            {
+                _selectedTransaction = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public HomeViewModel()
+        {
+            LogoutCommand = new RelayCommand<object>(null, Logout);
+            EditCommand = new RelayCommand<object>(CanEditTransaction, EditTransaction);
+        }
+
+        private void Logout(object parameter)
+        {
+            MessageBox.Show("Đăng xuất thành công!");
+        }
+
+        private void EditTransaction(object parameter)
+        {
+            if (SelectedTransaction != null)
+            {
+                EditTransactionView editView = new EditTransactionView(SelectedTransaction);
+                editView.ShowDialog();
+            }
+        }
+
+        private bool CanEditTransaction(object parameter)
+        {
+            return SelectedTransaction != null;
+        }
+    }
+}
